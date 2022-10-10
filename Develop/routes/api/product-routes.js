2fd5be2bc9f5d0e325b7ab2d88e9b141f3e8e3b4
Darrell Thomas <darrellthomas@Darrells-MacBook-Pro.local>
2022-10-10 18:06:55 -0400

    update

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -128,7 +128,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  roduct.destroy({
+  Product.destroy({
     where: {
       id: req.params.id
     }
